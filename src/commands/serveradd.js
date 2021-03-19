7343e63d8bf1426e739190e6aa507dd9caf4a6b2
Leo LECLERC <leoleclercleo@gmail.com>
2021-03-19 18:17:44 +0100

    fixed a few issues

--- src/commands/serveradd.js
+++ src/commands/serveradd.js
@@ -10,8 +10,14 @@ module.exports = {
 	aliases: ['sa','sadd','servera'],
 	execute(message, args, client, baselocation, command) {
 		message.delete({timeout:10}).catch(err=>{return})
-		if(args.length<2){message.channel.send('Require at least the id of the message and the name of the server or the ip:port').then(msg=>{msg.delete({timeout:2500})})}
+		if(args.length<2){message.channel.send('Require at least the id of the message and the name of the server or the ip:port').then(msg=>{msg.delete({timeout:5000})})}
 		let ipSave="",portSave=""
+		for(let i=1;i<args.length;i++){
+			if(args[i].split(":").length!=2){
+				message.channel.send("IP(s) must be with the format: x.x.x.x:p, example: 170.15.26.248:27018").then(msg=>{msg.delete({timeout:5000})}).catch(err=>{return})
+				return
+			}
+		}
 		for(let i=1;i<args.length;i++){
 			if(!(args[i]=='' || args[i]==' ' || args[i]==undefined)){
 				ipSave += args[i].split(":")[0]
@@ -26,7 +32,7 @@ module.exports = {
 				db.run(`UPDATE InformationMessage SET ipSave=?,portSave=? WHERE messageid=? AND channelid=?`,[rows[0].ipSave+'#'+ipSave,rows[0].portSave+'#'+portSave,args[0],message.channel.id])
 			}
 			else{
-				message.channel.send("Found no message with this ID in this channel").then(msg=>{msg.delete({timeout:2500})})
+				message.channel.send("Found no message with this ID in this channel").then(msg=>{msg.delete({timeout:2500})}).catch(err=>{return})
 			}
 		})
 		db.close()
